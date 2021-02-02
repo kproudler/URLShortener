72c43c34ee7cbb494c37e50e7e54a2d084c7e2a6
kproudler <kevinproudler@gmail.com>
2021-02-02 10:22:46 -0500

    added associations between user and shortened url

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,30 @@
 class ShortenedUrl < ApplicationRecord
+
+    validates :short_url, uniqueness: true
+    validates :long_url, :short_url, :user_id, presence: true
+
+    belongs_to :user,
+        primary_key: :id,
+        class_name: :User,
+        foreign_key: :user_id
+    
+    
+
+    def self.random_code
+        loop do
+            code = SecureRandom::urlsafe_base64(16)
+            return code unless ShortenedUrl.exists?(short_url: code)
+        end
+    end
+
+    def self.create_url(user, long_url)
+        ShortenedUrl.create!(
+            user_id: user.id,
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code
+        )
+    end
+
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
 
-    validates :email, presence: true
-    validates :email, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
+    has_many :submitted_urls,
+        primary_key: :id,    
+        class_name: :ShortenedUrl,
+        foreign_key: :user_id
+        
 
 end
